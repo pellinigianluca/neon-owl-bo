a01474bac69c1de9c99cee04d15bdd08a1d6fa19
[dyad] <pellini.gianluca@itiscannizzaro.edu.it>
2026-01-29 12:58:17 +0100

    Reverted all changes back to version 0df4c380712225ffd962191c6e956437d39d1628

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import MedievalGames from "./pages/MedievalGames";
+import MedievalGames from "./pages/MedievalGames"; // Import the new page
 
 const queryClient = new QueryClient();
 
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/medieval-games" element={<MedievalGames />} />
+          <Route path="/medieval-games" element={<MedievalGames />} /> {/* New route */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
